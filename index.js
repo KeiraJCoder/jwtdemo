302cedca2f9d0c413e0c79617466d95fa29f33b4
Keira <KeiraJCoder@hotmail.com>
2021-08-26 10:46:58 +0100

    commit

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-
+const errorRouter = require(".routes/error");
 
 app.get("/", (req, res) => {
     res.status(200).json({msg: process.env});
 })
+
+app.use("*", errorRouter);
+
 app.listen(process.env.HTTP_PORT || 5000, () => {
     console.log("HTTP Server Started");
 })
